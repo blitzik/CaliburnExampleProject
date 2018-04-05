3ad11bef88ae0e26e5ad7f06c6588c4c3a31f20c
Aleš Tichava <ales.tichava@gmail.com>
2018-04-05 18:43:39 +0200

    Vytvořena EventAggregator message "ChangeViewMessage", která nese jméno view modelu, na jehož pohled se budeme chtít přepnout

---
+++ CaliburnExample/CaliburnExample/EventAggregator/Messages/ChangeViewMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnExample.EventAggregator.Messages
+{
+    public class ChangeViewMessage
+    {
+        private string _viewModelName;
+        public string ViewModelName
+        {
+            get { return _viewModelName; }
+        }
+
+
+        public ChangeViewMessage(string viewName)
+        {
+            _viewModelName = viewName;
+        }
+    }
+}
