30cb17bc48b24ca9e36d6b9b5ac94f8002fe9976
Aleš Tichava <ales.tichava@gmail.com>
2018-04-05 18:42:03 +0200

    Přidána služba ViewModelResolver, která má za úkol načítat view modely podle názvu

---
+++ CaliburnExample/CaliburnExample/Services/ViewModelResolver/ViewModelResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaliburnExample.Views;
+using Caliburn.Micro;
+
+namespace CaliburnExample.Services.ViewModelResolver
+{
+    // Třída, která nám podle klíče (názvu ViewModelu) vrátí instanci konkrétního ViewModelu
+    public class ViewModelResolver : IViewModelResolver<Views.IViewModel>
+    {
+        private readonly SimpleContainer _container;
+
+
+        public ViewModelResolver(SimpleContainer container)
+        {
+            _container = container;
+        }
+
+
+        public IViewModel Resolve(string viewModel)
+        {
+            return (IViewModel)_container.GetInstance(Type.GetType(viewModel), viewModel);
+        }
+    }
+}
