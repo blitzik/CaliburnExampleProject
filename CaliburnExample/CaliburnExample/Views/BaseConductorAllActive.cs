030556bcbd57e0429f06b25f3d0ef26f74f95944
Aleš Tichava <ales.tichava@gmail.com>
2018-04-08 08:30:12 +0200

    Added All Active base conductor

---
+++ CaliburnExample/CaliburnExample/Views/BaseConductorAllActive.cs
@@ -0,0 +1,53 @@
+﻿using Caliburn.Micro;
+using CaliburnExample.Services.ViewModelResolver;
+using System;
+
+namespace CaliburnExample.Views
+{
+    public abstract class BaseConductorAllActive : Conductor<IViewModel>.Collection.AllActive, IViewModel
+    {
+        // property injection
+        private IEventAggregator _eventAggregator;
+        public IEventAggregator EventAggregator
+        {
+            get { return _eventAggregator; }
+            set { _eventAggregator = value; }
+        }
+
+
+        // property injection
+        private IViewModelResolver<IViewModel> _viewModelResolver;
+        public IViewModelResolver<IViewModel> ViewModelResolver
+        {
+            get { return _viewModelResolver; }
+            set { _viewModelResolver = value; }
+        }
+
+
+        public BaseConductorAllActive()
+        {
+            
+        }
+        
+
+        protected IViewModel AddViewModel(string viewModelName)
+        {
+            IViewModel vm = GetViewModel(viewModelName);
+            Items.Add(vm);
+
+            return vm;
+        }
+
+
+        protected IViewModel GetViewModel(string viewModelName)
+        {
+            IViewModel vm = ViewModelResolver.Resolve(viewModelName);
+            if (vm == null) {
+                throw new Exception("Requested ViewModel does not Exist!");
+            }
+
+            return vm;
+        }
+
+    }
+}
