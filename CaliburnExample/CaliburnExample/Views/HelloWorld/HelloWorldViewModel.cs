b6cda194d1117b43a8cf442c27c1572692b8bd36
Aleš Tichava <ales.tichava@gmail.com>
2018-04-15 12:14:03 +0200

    added comments on validation

--- CaliburnExample/CaliburnExample/Views/HelloWorld/HelloWorldViewModel.cs
+++ CaliburnExample/CaliburnExample/Views/HelloWorld/HelloWorldViewModel.cs
@@ -18,7 +18,9 @@ namespace CaliburnExample.Views.HelloWorld
                 ClearMessages();
                 int result;
                 if (!int.TryParse(value, out result)) {
-                    AddMessage("Age must be an integer number!", Severity.INFO);
+                    AddMessage("Age must be an integer number!"); // povinný je pouze první parametr
+                    // AddMessage("Age must be an integer number!", Severity.INFO); // Druhý parametr je defaultně nastaven na Severity.Error
+                    // AddMessage("Age must be an integer number!", Severity.INFO, nameof(Age)); // třetí parametr nemusíš psát, pokud se zpráva vztahuje k property, kde tu metodu voláš
                 }
 
                 _age = value;
