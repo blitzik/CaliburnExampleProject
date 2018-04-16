6657ed5c60b29c87f0a83eca76dedb1eae45c4b9
Aleš Tichava <ales.tichava@gmail.com>
2018-04-16 19:23:17 +0200

    Refactoring

--- CaliburnExample/CaliburnExample/Views/BaseConductorOneActive.cs
+++ CaliburnExample/CaliburnExample/Views/BaseConductorOneActive.cs
@@ -29,42 +29,17 @@ namespace CaliburnExample.Views
         }
 
 
-        protected Dictionary<string, IViewModel> _viewModels = new Dictionary<string, IViewModel>();
-
-
         public void DisplayView(string viewModelName)
         {
             ActivateItem(GetViewModel(viewModelName));
         }
 
 
-        public override void ActivateItem(IViewModel item)
-        {
-            if (ActiveItem == item) {
-                return;
-            }
-
-            string typeName = item.GetType().Name;
-            if (!_viewModels.ContainsKey(typeName)) {
-                _viewModels.Add(typeName, item);
-            }
-
-            base.ActivateItem(item);
-        }
-
-
         private IViewModel GetViewModel(string viewModelName)
         {
-            IViewModel viewModel;
-            if (!_viewModels.ContainsKey(viewModelName)) {
-                viewModel = _viewModelResolver.Resolve(viewModelName);
-                if (viewModel == null) {
-                    throw new Exception("Requested ViewModel does not Exist!");
-                }
-                _viewModels.Add(viewModelName, viewModel);
-
-            } else {
-                viewModel = _viewModels[viewModelName];
+            IViewModel viewModel = _viewModelResolver.Resolve(viewModelName);
+            if (viewModel == null) {
+                throw new Exception("Requested ViewModel does not Exist!");
             }
 
             return viewModel;
